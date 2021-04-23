158ab019f55f14e84313ee2aa26e4f9aaada47d3
PatrickDCullen <cullenpatrickd@gmail.com>
2021-04-23 14:52:51 -0400

    End of Ep 7: Make a Route and Link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First Post</a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores delectus minima distinctio obcaecati quas quia consequatur quam natus voluptas porro ex, voluptate tenetur quod illum ea modi quibusdam odio. Architecto.
+            Animi dolorem recusandae voluptatum pariatur impedit vero magnam adipisci culpa voluptas laboriosam repellat cupiditate veniam iste fugiat delectus corrupti, nobis vitae mollitia sunt reiciendis ut ratione. Non vitae deserunt culpa!
+            Totam itaque temporibus necessitatibus, neque sapiente, obcaecati, a culpa vero amet labore iste sequi eius tenetur. Iure necessitatibus repudiandae asperiores vero aliquid, possimus autem tempora at, libero voluptatibus, fugiat voluptates!
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post">My Second Post</a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores delectus minima distinctio obcaecati quas quia consequatur quam natus voluptas porro ex, voluptate tenetur quod illum ea modi quibusdam odio. Architecto.
+            Animi dolorem recusandae voluptatum pariatur impedit vero magnam adipisci culpa voluptas laboriosam repellat cupiditate veniam iste fugiat delectus corrupti, nobis vitae mollitia sunt reiciendis ut ratione. Non vitae deserunt culpa!
+            Totam itaque temporibus necessitatibus, neque sapiente, obcaecati, a culpa vero amet labore iste sequi eius tenetur. Iure necessitatibus repudiandae asperiores vero aliquid, possimus autem tempora at, libero voluptatibus, fugiat voluptates!
+        </p>
+    </article>
+
+    <article>
+        <h1>
+            <a href="/post">My Third Post</a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores delectus minima distinctio obcaecati quas quia consequatur quam natus voluptas porro ex, voluptate tenetur quod illum ea modi quibusdam odio. Architecto.
+            Animi dolorem recusandae voluptatum pariatur impedit vero magnam adipisci culpa voluptas laboriosam repellat cupiditate veniam iste fugiat delectus corrupti, nobis vitae mollitia sunt reiciendis ut ratione. Non vitae deserunt culpa!
+            Totam itaque temporibus necessitatibus, neque sapiente, obcaecati, a culpa vero amet labore iste sequi eius tenetur. Iure necessitatibus repudiandae asperiores vero aliquid, possimus autem tempora at, libero voluptatibus, fugiat voluptates!
+        </p>
+    </article>
+</body>
--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-
-<title>My blog</title>
-<link rel="stylesheet" href="/app.css">
-<script src="/app.js"></script>
-
-<body>
-    <h1>Hello World</h1>
-</body>
