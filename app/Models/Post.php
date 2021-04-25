9a7d70ef7f5d081654831aebc652e351e60429a8
PatrickDCullen <cullenpatrickd@gmail.com>
2021-04-25 15:31:36 -0400

    End Ep 13: Collection Sorting and Caching Refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -29,15 +29,19 @@ class Post
 
     public static function all()
     {
-        return collect(File::files(resource_path("posts")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document) => new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        ));
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
+
     }
 
     public static function find($slug)
