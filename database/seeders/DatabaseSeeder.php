a93b4401f48baefbb47b5ce0c97db9a3464c40ac
PatrickDCullen <cullenpatrickd@gmail.com>
2021-04-26 16:33:33 -0400

    End Ep 28: Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,47 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => '<p>lorem ipsum dolar</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt ab iusto, quaerat, modi accusantium incidunt doloribus eligendi voluptas atque qui laudantium soluta repellendus quo culpa, quam ratione repudiandae dignissimos?
-            Ipsam reiciendis hic laboriosam dolorem sint ea ipsa, numquam voluptatem voluptatum natus provident ipsum id officia libero consequuntur nihil officiis non, quos quasi deserunt iusto impedit. Sed neque atque officiis.
-            Cumque sequi aut vitae magnam harum nisi architecto, neque enim, praesentium animi libero. Ipsum, fuga nihil ea accusamus earum fugiat aspernatur, quo neque vel, voluptate nam impedit a? Veritatis, quisquam!</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>lorem ipsum dolar</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt ab iusto, quaerat, modi accusantium incidunt doloribus eligendi voluptas atque qui laudantium soluta repellendus quo culpa, quam ratione repudiandae dignissimos?
-            Ipsam reiciendis hic laboriosam dolorem sint ea ipsa, numquam voluptatem voluptatum natus provident ipsum id officia libero consequuntur nihil officiis non, quos quasi deserunt iusto impedit. Sed neque atque officiis.
-            Cumque sequi aut vitae magnam harum nisi architecto, neque enim, praesentium animi libero. Ipsum, fuga nihil ea accusamus earum fugiat aspernatur, quo neque vel, voluptate nam impedit a? Veritatis, quisquam!</p>'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
