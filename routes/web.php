7d733a26d3dac4c60c0c63ec7f4687ba44d155da
PatrickDCullen <cullenpatrickd@gmail.com>
2021-04-26 15:49:58 -0400

    Clockwork, and the N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
