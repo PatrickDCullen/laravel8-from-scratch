70ce09425690d9e89be62568c81e462f65c2416b
PatrickDCullen <cullenpatrickd@gmail.com>
2021-04-26 16:15:39 -0400

    End Ep 27: Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => '<p>lorem ipsum dolar</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt ab iusto, quaerat, modi accusantium incidunt doloribus eligendi voluptas atque qui laudantium soluta repellendus quo culpa, quam ratione repudiandae dignissimos?
+            Ipsam reiciendis hic laboriosam dolorem sint ea ipsa, numquam voluptatem voluptatum natus provident ipsum id officia libero consequuntur nihil officiis non, quos quasi deserunt iusto impedit. Sed neque atque officiis.
+            Cumque sequi aut vitae magnam harum nisi architecto, neque enim, praesentium animi libero. Ipsum, fuga nihil ea accusamus earum fugiat aspernatur, quo neque vel, voluptate nam impedit a? Veritatis, quisquam!</p>'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => '<p>lorem ipsum dolar</p>',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nesciunt ab iusto, quaerat, modi accusantium incidunt doloribus eligendi voluptas atque qui laudantium soluta repellendus quo culpa, quam ratione repudiandae dignissimos?
+            Ipsam reiciendis hic laboriosam dolorem sint ea ipsa, numquam voluptatem voluptatum natus provident ipsum id officia libero consequuntur nihil officiis non, quos quasi deserunt iusto impedit. Sed neque atque officiis.
+            Cumque sequi aut vitae magnam harum nisi architecto, neque enim, praesentium animi libero. Ipsum, fuga nihil ea accusamus earum fugiat aspernatur, quo neque vel, voluptate nam impedit a? Veritatis, quisquam!</p>'
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
         <h1>{{ $post->title }}</h1>
 
         <p>
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
 
         <div>
