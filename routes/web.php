4be276e76769fe5028b387d69d7aa2c8dd22a726
PatrickDCullen <cullenpatrickd@gmail.com>
2021-05-03 12:56:46 -0400

    End Ep 30: Eager Load Relationships on an Existing Model

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
