06b4feac126d99380cdfc66a4e0ddeaea2fbc05d
PatrickDCullen <cullenpatrickd@gmail.com>
2021-05-18 19:06:24 -0400

    End Ep 35: How to Extract a Dropdown Blade Component

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
@@ -35,7 +35,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
