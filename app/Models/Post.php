ed15cdced23ada76db519b3e43411e004ec072da
PatrickDCullen <cullenpatrickd@gmail.com>
2021-05-28 13:02:47 -0400

    End Ep 43: Fix a Confusing Eloquent Bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,9 +16,10 @@ class Post extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when(isset($filters['search']) ? $filters['search'] : false, function ($query, $search) {
-            $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%');
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+            );
         });
 
         $query->when(isset($filters['category']) ? $filters['category'] : false, function ($query, $category) {
