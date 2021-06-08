e3804777ab31ec4aad9a977819461a446e7169a6
PatrickDCullen <cullenpatrickd@gmail.com>
2021-06-08 14:11:42 -0400

    End Ep 45: Build a Register User Page

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|max:255|min:3',
+            'email' => 'required|max:255|email',
+            'password' => 'required|max:255|min:7',
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
+    }
+}
