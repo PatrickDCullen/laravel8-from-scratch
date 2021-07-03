192a2a2bda85fb887b1281aba84b77a7f6671279
PatrickDCullen <cullenpatrickd@gmail.com>
2021-07-03 13:40:30 -0400

    End Ep 56: Activate the Contact Form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,25 +51,35 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    <x-panel>
-                        <form method="POST" action="#">
-                            @csrf
+                    @auth
+                        <x-panel>
+                            <form method="POST" action="/posts/{{ $post->slug }}/comments">
+                                @csrf
 
-                            <header class="flex items-center">
-                                <img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+                                <header class="flex items-center">
+                                    <img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
 
-                                <h2 class="ml-4">Want to participate?</h2>
-                            </header>
+                                    <h2 class="ml-4">Want to participate?</h2>
+                                </header>
 
-                            <div class="mt-6">
-                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, think of something to say!"></textarea>
-                            </div>
+                                <div class="mt-6">
+                                    <textarea
+                                        name="body"
+                                        class="w-full text-sm focus:outline-none focus:ring"
+                                        rows="5"
+                                        placeholder="Quick, think of something to say!"></textarea>
+                                </div>
 
-                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
-                        </form>
-                    </x-panel>
+                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                                </div>
+                            </form>
+                        </x-panel>
+                    @else
+                        <p class="font-semibold">
+                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment.
+                        </p>
+                        @endauth
 
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\PostCommentsController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
+
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
