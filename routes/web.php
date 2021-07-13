17a6d836fafe411c64035ddb040b5881f26c0393
PatrickDCullen <cullenpatrickd@gmail.com>
2021-07-13 13:06:23 -0400

    End Ep 58: Mailchimp API Tinkering

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us6'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('7ca2a14957', [
+        'email_address' => 'cullenpatrickd@gmail.com',
+        'status' => 'subscribed',
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
