a7ecd783b3e4ef17d0be0de04e48e9080069332d
PatrickDCullen <cullenpatrickd@gmail.com>
2021-08-02 17:45:12 -0400

    End Ep 66: Extend the Admin Layout

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,91 +1,17 @@
 <x-layout>
     <section class="px-6 py-8">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register!</h1>
-
-            <form method="POST" action="/register" class="mt-10">
-                @csrf
-
-                <div class="mb-6">
-                    <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Name
-                    </label>
-
-                    <input
-                        type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="name"
-                        id="name"
-                        value="{{ old('name') }}"
-                        required
-                    >
-
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Username
-                    </label>
-
-                    <input
-                        type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="username"
-                        id="username"
-                        value="{{ old('username') }}"
-                        required
-                    >
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Email
-                    </label>
-
-                    <input
-                        type="email"
-                        class="border border-gray-400 p-2 w-full"
-                        name="email"
-                        id="email"
-                        value="{{ old('email') }}"
-                        required
-                    >
-
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                    Password
-                    </label>
-
-                    <input
-                        type="password"
-                        class="border border-gray-400 p-2 w-full"
-                        name="password"
-                        id="password"
-                        required
-                    >
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
-                </div>
-
-            </form>
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register!</h1>
+                <form method="POST" action="/register" class="mt-10">
+                    @csrf
+                    <x-form.input name="name" type="text" />
+                    <x-form.input name="username" type="text" />
+                    <x-form.input name="email" type="email" autocomplete="username"/>
+                    <x-form.input name="password" type="password" autocomplete="new-password" />
+                    <x-form.button type="submit">Submit</x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
